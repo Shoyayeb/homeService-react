098834a76c083133e0051fa3907b3d7ded9ec6f7
Shoyayeb <sa.fine@outlook.com>
2022-02-07 01:51:49 +0600

    dashboard stylings and table for orders

--- src/Pages/DashBoard/DashBoardNav/DashBoardNav.tsx
+++ src/Pages/DashBoard/DashBoardNav/DashBoardNav.tsx
@@ -3,13 +3,12 @@ import React from "react";
 import { Link, LinkProps, useMatch, useResolvedPath } from "react-router-dom";
 
 const DashBoardNav = () => {
-  function CustomLink({ children, to, ...props }: LinkProps) {
+  
+  const CustomLink = ({ children, to, ...props }: LinkProps) => {
     let resolved = useResolvedPath(to);
     let match = useMatch({ path: resolved.pathname, end: true });
-
     return (
       <Link
-        // style={{ textDecoration: match ? "underline" : "none" }}
         className={
           match
             ? "w-full text-gray-800 dark:text-white flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start border-l-4 border-purple-500"
@@ -21,81 +20,79 @@ const DashBoardNav = () => {
         {children}
       </Link>
     );
-  }
+  };
   return (
-    <div className="flex items-start justify-between">
-      <div className="h-screen hidden lg:block shadow-lg relative w-80">
-        <div className="bg-white h-full dark:bg-gray-700">
-          <div className="flex items-center justify-start pt-6 ml-8">
-            <p className="font-bold dark:text-white text-xl">Home Service</p>
-          </div>
-          <nav className="mt-6">
-            <div>
-              <CustomLink to="/dashboard">
-                <span className="text-left">
-                  <PresentationChartLineIcon width="20" height="20" />
-                </span>
-                <span className="mx-2 text-sm font-normal">Dashboard</span>
-              </CustomLink>
-              <CustomLink
-                // className="w-full text-gray-400 flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start hover:text-gray-800 border-l-4 border-transparent"
-                to="/dashboard/users"
-              >
-                <span className="text-left">
-                  <svg
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    viewBox="0 0 2048 1792"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M1070 1178l306-564h-654l-306 564h654zm722-282q0 182-71 348t-191 286-286 191-348 71-348-71-286-191-191-286-71-348 71-348 191-286 286-191 348-71 348 71 286 191 191 286 71 348z"></path>
-                  </svg>
-                </span>
-                <span className="mx-2 text-sm font-normal">
-                  Refered Projects
-                  <span className="p-1 ml-4 rounded-lg w-4 h-2 bg-gray-200 text-gray-400 text-xs">
-                    0
-                  </span>
-                </span>
-              </CustomLink>
-              <Link
-                className="w-full text-gray-400 flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start hover:text-gray-800 border-l-4 border-transparent"
-                to="/"
-              >
-                <span className="text-left">
-                  <svg
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    viewBox="0 0 1792 1792"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M1728 608v704q0 92-66 158t-158 66h-1216q-92 0-158-66t-66-158v-960q0-92 66-158t158-66h320q92 0 158 66t66 158v32h672q92 0 158 66t66 158z"></path>
-                  </svg>
-                </span>
-                <span className="mx-4 text-sm font-normal">Resources</span>
-              </Link>
-              <Link
-                className="w-full text-gray-400 flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start hover:text-gray-800 border-l-4 border-transparent"
-                to="/dashboard/users"
-              >
-                <span className="text-left">
-                  <svg
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    viewBox="0 0 2048 1792"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M580 461q0-41-25-66t-66-25q-43 0-76 25.5t-33 65.5q0 39 33 64.5t76 25.5q41 0 66-24.5t25-65.5zm743 507q0-28-25.5-50t-65.5-22q-27 0-49.5 22.5t-22.5 49.5q0 28 22.5 50.5t49.5 22.5q40 0 65.5-22t25.5-51zm-236-507q0-41-24.5-66t-65.5-25q-43 0-76 25.5t-33 65.5q0 39 33 64.5t76 25.5q41 0 65.5-24.5t24.5-65.5zm635 507q0-28-26-50t-65-22q-27 0-49.5 22.5t-22.5 49.5q0 28 22.5 50.5t49.5 22.5q39 0 65-22t26-51zm-266-397q-31-4-70-4-169 0-311 77t-223.5 208.5-81.5 287.5q0 78 23 152-35 3-68 3-26 0-50-1.5t-55-6.5-44.5-7-54.5-10.5-50-10.5l-253 127 72-218q-290-203-290-490 0-169 97.5-311t264-223.5 363.5-81.5q176 0 332.5 66t262 182.5 136.5 260.5zm592 561q0 117-68.5 223.5t-185.5 193.5l55 181-199-109q-150 37-218 37-169 0-311-70.5t-223.5-191.5-81.5-264 81.5-264 223.5-191.5 311-70.5q161 0 303 70.5t227.5 192 85.5 263.5z"></path>
-                  </svg>
-                </span>
-                <span className="mx-4 text-sm font-normal">Store feedback</span>
-              </Link>
-            </div>
-          </nav>
+    <div className="h-screen hidden lg:block shadow-lg relative w-80">
+      <div className="bg-white h-full dark:bg-gray-700">
+        <div className="flex items-center justify-start pt-6 ml-8">
+          <p className="font-bold dark:text-white text-xl">Home Service</p>
         </div>
+        <nav className="mt-6">
+          <div>
+            <CustomLink to="/dashboard">
+              <span className="text-left">
+                <PresentationChartLineIcon width="20" height="20" />
+              </span>
+              <span className="mx-2 text-sm font-normal">Dashboard</span>
+            </CustomLink>
+            <CustomLink
+              // className="w-full text-gray-400 flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start hover:text-gray-800 border-l-4 border-transparent"
+              to="/dashboard/users"
+            >
+              <span className="text-left">
+                <svg
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  viewBox="0 0 2048 1792"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M1070 1178l306-564h-654l-306 564h654zm722-282q0 182-71 348t-191 286-286 191-348 71-348-71-286-191-191-286-71-348 71-348 191-286 286-191 348-71 348 71 286 191 191 286 71 348z"></path>
+                </svg>
+              </span>
+              <span className="mx-2 text-sm font-normal">
+                Refered Projects
+                <span className="p-1 ml-4 rounded-lg w-4 h-2 bg-gray-200 text-gray-400 text-xs">
+                  0
+                </span>
+              </span>
+            </CustomLink>
+            <Link
+              className="w-full text-gray-400 flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start hover:text-gray-800 border-l-4 border-transparent"
+              to="/"
+            >
+              <span className="text-left">
+                <svg
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  viewBox="0 0 1792 1792"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M1728 608v704q0 92-66 158t-158 66h-1216q-92 0-158-66t-66-158v-960q0-92 66-158t158-66h320q92 0 158 66t66 158v32h672q92 0 158 66t66 158z"></path>
+                </svg>
+              </span>
+              <span className="mx-4 text-sm font-normal">Resources</span>
+            </Link>
+            <Link
+              className="w-full text-gray-400 flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start hover:text-gray-800 border-l-4 border-transparent"
+              to="/dashboard/users"
+            >
+              <span className="text-left">
+                <svg
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  viewBox="0 0 2048 1792"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M580 461q0-41-25-66t-66-25q-43 0-76 25.5t-33 65.5q0 39 33 64.5t76 25.5q41 0 66-24.5t25-65.5zm743 507q0-28-25.5-50t-65.5-22q-27 0-49.5 22.5t-22.5 49.5q0 28 22.5 50.5t49.5 22.5q40 0 65.5-22t25.5-51zm-236-507q0-41-24.5-66t-65.5-25q-43 0-76 25.5t-33 65.5q0 39 33 64.5t76 25.5q41 0 65.5-24.5t24.5-65.5zm635 507q0-28-26-50t-65-22q-27 0-49.5 22.5t-22.5 49.5q0 28 22.5 50.5t49.5 22.5q39 0 65-22t26-51zm-266-397q-31-4-70-4-169 0-311 77t-223.5 208.5-81.5 287.5q0 78 23 152-35 3-68 3-26 0-50-1.5t-55-6.5-44.5-7-54.5-10.5-50-10.5l-253 127 72-218q-290-203-290-490 0-169 97.5-311t264-223.5 363.5-81.5q176 0 332.5 66t262 182.5 136.5 260.5zm592 561q0 117-68.5 223.5t-185.5 193.5l55 181-199-109q-150 37-218 37-169 0-311-70.5t-223.5-191.5-81.5-264 81.5-264 223.5-191.5 311-70.5q161 0 303 70.5t227.5 192 85.5 263.5z"></path>
+                </svg>
+              </span>
+              <span className="mx-4 text-sm font-normal">Store feedback</span>
+            </Link>
+          </div>
+        </nav>
       </div>
     </div>
   );
